f6e529439ac16629ea0eded9423d34d05425642c
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2025-09-01 17:52:04 +0530

    dialect added:

--- src/config/config.js
+++ src/config/config.js
@@ -21,6 +21,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT || "mysql",
   }
 };
